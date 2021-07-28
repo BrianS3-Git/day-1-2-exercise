afdb4d51cba975e6c42064df9ed349d0d68d4cec
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-07-28 21:58:46 +0000

    Fixed missing string quote after Hello

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 
 print("Day 1 - String Manipulation\nString Concatenation is done with the \"+\" sign.")
 
-print("e.g. print(\"Hello \ + \"world\")\nNew lines can be created with a backslash and n.")
+print("e.g. print(\"Hello \" + \"world\")\nNew lines can be created with a backslash and n.")
 
