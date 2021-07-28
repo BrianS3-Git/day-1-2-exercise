f88812aeaf70ec1837d05a19e619fcacd21181ca
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-07-28 21:49:19 +0000

    Corrected + sign to be a string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Fix the code below 👇
 
-print("Day 1 - String Manipulation\nString Concatenation is done with the "+" sign.")
+print("Day 1 - String Manipulation\nString Concatenation is done with the \"+\" sign.")
 
 print("e.g. print(\"Hello \ + \"world\")\nNew lines can be created with a backslash and n.")
 
